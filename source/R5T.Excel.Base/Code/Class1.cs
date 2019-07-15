1d5dc9958f4233068932eb20dee9ca0e36c584a0
MinexAutomation <minex.automation@gmail.com>
2019-07-15 16:09:24 -0400

    Added ExcelHelper functionality to automatically open an Excel file in Excel (for human review).

--- source/R5T.Excel.Base/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Excel.Base
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Excel.Base/Code/Helpers/ExcelHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace R5T.Excel
+{
+    public static class ExcelHelper
+    {
+        public const string ExcelExecutableCommandValue = @"EXCEL.EXE";
+
+
+        public static void OpenInExcel(string excelFilePath)
+        {
+            var arguments = $@"""{excelFilePath}"""; // Surround in quotes.
+
+            Process.Start(ExcelHelper.ExcelExecutableCommandValue, arguments);
+        }
+    }
+}
